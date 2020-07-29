a91dae052d0e7c2147f6eefc22061cc4d27e80f4
Michal Stepanek <micstepanek@gmail.com>
2020-07-29 10:45:50 +0200

    Test Con's monkey patch

--- test_i3tags.py
+++ test_i3tags.py
@@ -3,3 +3,10 @@ from i3tags import *
 
 def test_tags():
     assert i3.tags is i3._tag_tree.nodes[1].nodes[1].nodes
+
+class TestI3ipcConMonkeyPatch:
+    def test_remove_focus_on_windows(self):
+        windows = i3.get_tree().leaves()
+        for window in windows:
+            window.remove_focus()
+            assert window.focused == False
