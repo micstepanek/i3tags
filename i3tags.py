b9a65d56754d58aba1c6f56c7c4896a71840b493
Michal Stepanek <micstepanek@gmail.com>
2020-07-31 18:29:55 +0200

    Stop mixing business logic with i3ipc.Connection class

--- i3tags.py
+++ i3tags.py
@@ -7,7 +7,158 @@ import subprocess
 import copy
 
 
-class TkWrapper(tkinter.Tk):
+class BusinessLogic:
+    def __init__(self):
+        i3.on(i3ipc.Event.BINDING, self.handle_binding)
+        self._tag_tree = i3.get_tree()
+        self.previous_tag = self._tag_tree.find_focused().workspace().name
+        #aliases
+        self.listen_for_bindings = i3.main
+        self.stop_listening = i3.main_quit
+
+    @property
+    def tags(self):
+        return self._tag_tree.nodes[1].nodes[1].nodes
+
+    @tags.setter
+    def tags(self, list_):
+        self._tag_tree.nodes[1].nodes[1].nodes = list_
+
+    def handle_binding(self, _, binding_event):
+        command = binding_event.binding.command
+        print(command)
+        key = binding_event.binding.symbol
+        if 'mode default' in command:
+            gui.reset()
+            if command.startswith('mode tag'):
+                self.switch_tag(self.find_target(key))
+            elif command.startswith('mode branch'):
+                self.branch_windows(key)
+        elif command == 'mode henkan':
+            self._update_tag_tree()
+            gui.activate(self._tag_tree)
+        elif command.endswith('title window'):
+            self.prepare_for_entry()
+            gui.show_rename_entry()
+        elif command.endswith('retag window'):
+            self.prepare_for_entry()
+            gui.show_tag_entry()
+        else:
+            gui.show_mode(command)
+
+    def branch_windows(self, tag_name):
+        current_tag = self._tag_tree.find_focused().tag()
+        new_tag = copy.copy(current_tag)
+        new_tag.name = tag_name
+        self.tags.append(new_tag)
+        i3.command(f'move window to workspace {tag_name}')
+
+    def prepare_for_entry(self):
+        self.stop_listening()
+        i3.command('mode default')
+
+    def find_target(self, key):
+        current_tag = self._tag_tree.find_focused().workspace().name
+        if current_tag == key:
+            target = self.previous_tag
+        else:
+            target = key
+        self.previous_tag = current_tag
+        return target
+
+    def switch_tag(self, target):
+        #for tag in self.tags:
+        #    if tag.name == target:
+        #        for window in tag.nodes:
+        #            self.command(f'[con_id={window.id}]move window to workspace tmp')
+        #            self.command(f'[con_id={window.id}]move window to workspace {target}')
+        #        break
+        #self.command(f'workspace {target}')
+        # - blocked by PyCharm if going to empty workspace
+        # subprocess with i3-msg works
+        subprocess.run(['i3-msg', 'workspace', target])
+
+    def _update_tag_tree(self):
+        self.workspace_tree = i3.get_tree()
+        self._inspect_tag_tree()
+        self._inspect_workspaces()
+        self._inspect_windows()
+        self.tags.sort(key=lambda x: x.name)
+
+    def process_tag_entry(self, entry):
+        if entry == 'quit':
+            gui.destroy()
+            exit()
+        current_tag = self._tag_tree.find_focused().workspace()
+        current_window = self._tag_tree.find_focused()
+        self._tag_tree.remove_node_by_id(current_window.id)
+        if entry == '':
+            i3.command('kill') #kill focused window
+        else:
+            change_workspace_later = False
+            for char in entry:
+                if char == '.':
+                    change_workspace_later = True
+                    break
+                else:
+                    placed = self._add_to_existing_tag(char, current_window)
+                if not placed:
+                    new_tag = copy.copy(current_tag)
+                    new_tag.name = char
+                    new_tag.nodes = [current_window]
+                    self.tags.append(new_tag)
+            if change_workspace_later:
+                self.switch_tag(entry[0])
+            elif current_tag.name not in entry:
+                i3.command('move window to workspace {}'.format(entry[0]))
+
+    def _add_to_existing_tag(self, char, current_window):
+        for tag in self.tags:
+            if char == tag.name:
+                tag.nodes.append(current_window)
+                return True
+        return False
+
+    def retitle_focused_window(self, title):
+        focused_window = self._tag_tree.find_focused()
+        for window in self._tag_tree.leaves():
+            if focused_window.id == window.id:
+                window.name = title
+                window.window_title = title
+        subprocess.run(['xdotool',
+                        'set_window',
+                        '--name',
+                        title,
+                        str(self._tag_tree.find_focused().window)
+                        ])
+
+    def _inspect_tag_tree(self):
+        current_workspace = self.workspace_tree.find_focused().workspace()
+        tags = [
+                current_workspace if current_workspace.name == tag.name else
+                tag.update_tag(self.workspace_tree)
+                for tag in self.tags
+                    ]
+        self.tags = [tag for tag in tags if tag.nodes]
+
+    def _inspect_workspaces(self):
+        tag_names = [tag.name for tag in self.tags]
+        for workspace in self.workspace_tree.workspaces():
+            if workspace.name not in tag_names:
+                self.tags.append(workspace)
+
+    def _inspect_windows(self):
+        tagged_window_IDs = [window.id for window in self._tag_tree.leaves()]
+        for window in self.workspace_tree.leaves():
+            if window.id not in tagged_window_IDs:
+                #copy window frow workspace to tag
+                workspace_id = (self.workspace_tree
+                                .find_by_id(window.id)
+                                .workspace().id)
+                self._tag_tree.find_by_id(workspace_id).nodes.append(window)
+
+
+class GraphicalUserInterfaceWrapper(tkinter.Tk):
     _FOCUS_COLOR = '#cfc'
     _URGENT_COLOR = 'yellow'
     _COLOR_0 = 'white'
@@ -88,7 +239,7 @@ class TkWrapper(tkinter.Tk):
         self.show_entry(self.process_rename_entry)
 
     def process_rename_entry(self, _):
-        i3.retitle_focused_window(self.entry.get())
+        logic.retitle_focused_window(self.entry.get())
         self.reset_after_entry()
 
     def show_tag_entry(self):
@@ -97,12 +248,12 @@ class TkWrapper(tkinter.Tk):
     def redirect_tag_entry(self, _):
         entry = self.entry.get()
         self.reset()
-        i3.process_tag_entry(entry)
-        i3.main()
+        logic.process_tag_entry(entry)
+        logic.listen_for_bindings()
 
     def reset_after_entry(self, _=None):
         self.reset()
-        i3.main()
+        logic.listen_for_bindings()
 
     def reset(self):
         self.clear()
@@ -120,155 +271,6 @@ class TkWrapper(tkinter.Tk):
         for hint in mode_hints:
             self.add_label(hint)
         self.update()
-
-
-class I3Wrapper(i3ipc.Connection):
-    def __init__(self):
-        super().__init__()
-        self.on(i3ipc.Event.BINDING, self.handle_mode)
-        self._tag_tree = self.get_tree()
-        self.previous_tag = self._tag_tree.find_focused().workspace().name
-
-    @property
-    def tags(self):
-        return self._tag_tree.nodes[1].nodes[1].nodes
-
-    @tags.setter
-    def tags(self, list_):
-        self._tag_tree.nodes[1].nodes[1].nodes = list_
-
-    def handle_mode(self, _, binding_event):
-        command = binding_event.binding.command
-        print(command)
-        key = binding_event.binding.symbol
-        if 'mode default' in command:
-            gui.reset()
-            if command.startswith('mode tag'):
-                self.switch_tag(self.find_target(key))
-            elif command.startswith('mode branch'):
-                self.branch_windows(key)
-        elif command == 'mode henkan':
-            self._update_tag_tree()
-            gui.activate(self._tag_tree)
-        elif command.endswith('title window'):
-            self.prepare_for_entry()
-            gui.show_rename_entry()
-        elif command.endswith('retag window'):
-            self.prepare_for_entry()
-            gui.show_tag_entry()
-        else:
-            gui.show_mode(command)
-
-    def branch_windows(self, tag_name):
-        current_tag = self._tag_tree.find_focused().tag()
-        new_tag = copy.copy(current_tag)
-        new_tag.name = tag_name
-        self.tags.append(new_tag)
-        i3.command(f'move window to workspace {tag_name}')
-
-    def prepare_for_entry(self):
-        self.main_quit()
-        self.command('mode default')
-
-    def find_target(self, key):
-        current_tag = self._tag_tree.find_focused().workspace().name
-        if current_tag == key:
-            target = self.previous_tag
-        else:
-            target = key
-        self.previous_tag = current_tag
-        return target
-
-    def switch_tag(self, target):
-        for tag in self.tags:
-            if tag.name == target:
-                for window in tag.nodes:
-                    self.command(f'[con_id={window.id}]move window to workspace tmp')
-                    self.command(f'[con_id={window.id}]move window to workspace {target}')
-                break
-        #self.command(f'workspace {target}')
-        # - blocked by PyCharm if going to empty workspace
-        # subprocess with i3-msg works
-        subprocess.run(['i3-msg', 'workspace', target])
-
-    def _update_tag_tree(self):
-        self.workspace_tree = self.get_tree()
-        self._inspect_tag_tree()
-        self._inspect_workspaces()
-        self._inspect_windows()
-        self.tags.sort(key=lambda x: x.name)
-
-    def process_tag_entry(self, entry):
-        if entry == 'quit':
-            gui.destroy()
-            exit()
-        current_tag = self._tag_tree.find_focused().workspace()
-        current_window = self._tag_tree.find_focused()
-        self._tag_tree.remove_node_by_id(current_window.id)
-        if entry == '':
-            self.command('kill') #kill focused window
-        else:
-            change_workspace_later = False
-            for char in entry:
-                if char == '.':
-                    change_workspace_later = True
-                    break
-                else:
-                    placed = self._add_to_existing_tag(char, current_window)
-                if not placed:
-                    new_tag = copy.copy(current_tag)
-                    new_tag.name = char
-                    new_tag.nodes = [current_window]
-                    self.tags.append(new_tag)
-            if change_workspace_later:
-                self.switch_tag(entry[0])
-            elif current_tag.name not in entry:
-                self.command('move window to workspace {}'.format(entry[0]))
-
-    def _add_to_existing_tag(self, char, current_window):
-        for tag in self.tags:
-            if char == tag.name:
-                tag.nodes.append(current_window)
-                return True
-        return False
-
-    def retitle_focused_window(self, title):
-        focused_window = self._tag_tree.find_focused()
-        for window in self._tag_tree.leaves():
-            if focused_window.id == window.id:
-                window.name = title
-                window.window_title = title
-        subprocess.run(['xdotool',
-                        'set_window',
-                        '--name',
-                        title,
-                        str(self._tag_tree.find_focused().window)
-                        ])
-
-    def _inspect_tag_tree(self):
-        current_workspace = self.workspace_tree.find_focused().workspace()
-        tags = [
-                current_workspace if current_workspace.name == tag.name else
-                tag.update_tag(self.workspace_tree)
-                for tag in self.tags
-                    ]
-        self.tags = [tag for tag in tags if tag.nodes]
-
-    def _inspect_workspaces(self):
-        tag_names = [tag.name for tag in self.tags]
-        for workspace in self.workspace_tree.workspaces():
-            if workspace.name not in tag_names:
-                self.tags.append(workspace)
-
-    def _inspect_windows(self):
-        tagged_window_IDs = [window.id for window in self._tag_tree.leaves()]
-        for window in self.workspace_tree.leaves():
-            if window.id not in tagged_window_IDs:
-                #copy window frow workspace to tag
-                workspace_id = (self.workspace_tree
-                                .find_by_id(window.id)
-                                .workspace().id)
-                self._tag_tree.find_by_id(workspace_id).nodes.append(window)
 
 
 class I3ipcConMonkeyPatch():
@@ -305,8 +307,9 @@ class I3ipcConMonkeyPatch():
     Con.remove_node_by_id = remove_node_by_id
 
 
-gui = TkWrapper()
-i3 = I3Wrapper()
+i3 = i3ipc.Connection()
+gui = GraphicalUserInterfaceWrapper()
+logic = BusinessLogic()
 if __name__ == '__main__':
-    i3.main()
+    logic.listen_for_bindings()
     gui.mainloop()
--- test_i3tags.py
+++ test_i3tags.py
@@ -2,16 +2,16 @@
 from i3tags import *
 
 def test_tags():
-    assert i3.tags is i3._tag_tree.nodes[1].nodes[1].nodes
+    assert logic.tags is logic._tag_tree.nodes[1].nodes[1].nodes
 
 class TestI3ipcConMonkeyPatch:
     def test_remove_focus_on_windows(self):
-        windows = i3.get_tree().leaves()
+        windows = logic.get_tree().leaves()
         for window in windows:
             window.remove_focus()
             assert window.focused == False
 
     def test_remove_focus_on_tags(self):
-        for tag in i3.tags:
+        for tag in logic.tags:
             tag.remove_focus()
             assert tag.focused == False
