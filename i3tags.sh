6316ed19cecd2e37b204d5be7f47b297d6469ff2
Michal Stepanek <micstepanek@gmail.com>
2020-09-28 21:13:01 +0200

    Remove hardcoded path to log

--- i3tags.sh
+++ i3tags.sh
@@ -4,6 +4,6 @@ if [ "$1" = "nolog" ]
     i3tags.py
 else
     # Print/write stdout and stderr to console and log
-    i3tags.py 2>&1 | tee -a ~/aa/bin/i3/i3tags.log
+    i3tags.py 2>&1 | tee -a i3tags.log
 fi
 
