786da3b49a95fe0efab1b4df3f8c1304347687df
Michal Stepanek <micstepanek@gmail.com>
2020-10-07 13:59:57 +0200

    Make i3tags.py independent from bash variable $PATH

--- i3tags.sh
+++ i3tags.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ "$1" = "nolog" ]
     then
-    i3tags.py
+    python3.8 i3tags.py
 else
     # Print/write stdout and stderr to console and log
-    i3tags.py 2>&1 | tee -a ~/.i3tags.log
+    python3.8 i3tags.py 2>&1 | tee -a ~/.i3tags.log
 fi
 
